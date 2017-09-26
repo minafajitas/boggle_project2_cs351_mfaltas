24a3ce5a5e985b0a4052ca5fa0248b893d06769d
Mina Faltas <iammina@unm.edu>
2017-09-26 12:16:47 -0600

    first version complete with gui

---
+++ src/Controller.java
@@ -0,0 +1,47 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+import static javafx.application.Application.launch;
+
+public class Controller extends Application
+{
+    public static void main(String[] args)
+    {
+      launch(args);
+    }
+
+
+
+
+  @Override
+  public void start(Stage primaryStage) throws Exception
+  {
+    Dictionary newDictionary = new Dictionary();
+
+    VBox mainMenu = new VBox();
+    TextField wordField = new TextField();
+    Button enterWord = new Button("enter");
+    mainMenu.getChildren().addAll(wordField, enterWord);
+    primaryStage.setScene(new Scene(mainMenu, 300,300));
+    primaryStage.show();
+
+    enterWord.setOnAction(new EventHandler<ActionEvent>()
+    {
+      @Override
+      public void handle(ActionEvent event)
+      {
+        String enteredText;
+        enteredText = wordField.getText();
+        System.out.println(newDictionary.inDictionary(enteredText));
+      }
+    });
+
+  }
+}
---
+++ src/Dictionary.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Dictionary
+{
+    ArrayList<String> wordList = new ArrayList<String>();
+
+    FileReader reader;
+    BufferedReader buffReader;
+
+    Dictionary()
+    {
+      try
+      {
+        reader = new FileReader("Assets/OpenEnglishWordList.txt");
+      } catch (FileNotFoundException e)
+      {
+        e.printStackTrace();
+        System.exit(1);
+      }
+
+      buffReader = new BufferedReader(reader);
+
+      String next = "";
+
+      while (true)
+      {
+        try
+        {
+          next = buffReader.readLine();
+        }
+        catch (IOException e)
+        {
+          e.printStackTrace();
+          System.out.println("error while reading");
+          System.exit(1);
+        }
+
+        if (next != null)
+        {
+          wordList.add(next);
+          continue;
+        }
+        break;
+      }
+    }
+
+  public boolean inDictionary (String word)
+  {
+    return wordList.contains(word);
+  }
+}
