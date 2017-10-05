703751b470d760ea221a039e95b407fb0f71f3f8
Mina Faltas <iammina@unm.edu>
2017-10-05 16:21:25 -0600

    8th: timer done

--- src/Controller.java
+++ src/Controller.java
@@ -1,3 +1,4 @@
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -17,82 +18,141 @@ import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 
-public class Controller extends Application {
-    public static void main(String[] args) {
-        launch(args);
+public class Controller extends Application
+{
+
+  public static void main(String[] args)
+  {
+    launch(args);
+  }
+
+  Board newboard = new Board();
+
+  private int score = 0;
+  Text timerText = new Text();
+
+  @Override
+  public void start(Stage primaryStage) throws Exception
+  {
+    Dictionary newDictionary = new Dictionary();
+
+    GridPane boardPane = new GridPane();
+
+
+    int col = 0;
+    int row = 0;
+    for (Die Dice : newboard.getBoard())
+    {
+      boardPane.add(Dice.getImageView(), col, row);
+      if (col > 2)
+      {
+        col = 0;
+        row++;
+      } else
+      {
+        col++;
+      }
     }
 
-    Board newboard = new Board();
+    BorderPane mainGamePane = new BorderPane();
+    VBox gameVbox = new VBox();
+    HBox gameTextHbox = new HBox();
+    HBox topHbox = new HBox();
 
-    private int score = 0;
+    VBox rightNumbers = new VBox();
+    VBox wrongNumbers = new VBox();
+    ScrollPane rightNumbersScroll = new ScrollPane();
+    ScrollPane wrongNumbersScroll = new ScrollPane();
+    Text rightNumbersText = new Text("Right Numbers: ");
+    rightNumbersText.setFont(Font.font(30));
+    Text wrongNumbersText = new Text("Wrong Numbers: ");
+    wrongNumbersText.setFont(Font.font(30));
+    rightNumbers.getChildren().add(rightNumbersText);
+    wrongNumbers.getChildren().add(wrongNumbersText);
+    rightNumbersScroll.setContent(rightNumbers);
+    wrongNumbersScroll.setContent(wrongNumbers);
 
+    Text scoreText = new Text("Score: " + score);
+    topHbox.getChildren().add(scoreText);
+    topHbox.getChildren().add(timerText);
+    topHbox.setAlignment(Pos.CENTER);
+    scoreText.setFont(Font.font(60));
+    TextField wordField = new TextField();
+    Button enterWord = new Button("enter");
+    gameVbox.getChildren().add(boardPane);
+    boardPane.setAlignment(Pos.CENTER);
+    gameTextHbox.getChildren().addAll(wordField, enterWord);
+    gameVbox.getChildren().add(gameTextHbox);
+    gameTextHbox.setAlignment(Pos.BASELINE_CENTER);
+    gameVbox.setAlignment(Pos.CENTER);
+    mainGamePane.setCenter(gameVbox);
+    mainGamePane.setTop(topHbox);
+    mainGamePane.setRight(rightNumbersScroll);
+    mainGamePane.setLeft(wrongNumbersScroll);
+
+    gameTimer newTimer = new gameTimer();
+    newTimer.start();
+
+    primaryStage.setScene(new Scene(mainGamePane, 1300, 800));
+    primaryStage.show();
+
+
+    enterWord.setOnAction(new EventHandler<ActionEvent>()
+    {
+      @Override
+      public void handle(ActionEvent event)
+      {
+        String enteredText;
+        enteredText = wordField.getText();
+        if (!enteredText.equals(""))
+        {
+          Text newWord = new Text(enteredText);
+          newWord.setFont(Font.font(20));
+          if (newboard.findWord(enteredText, 0, 0) && newDictionary.inDictionary(enteredText))
+          {
+            rightNumbers.getChildren().add(newWord);
+            score += enteredText.length() - 2;
+            scoreText.setText("Score: " + score);
+          } else
+          {
+            wrongNumbers.getChildren().add(newWord);
+          }
+          System.out.println(newboard.findWord(enteredText, 0, 0));
+          System.out.println(newDictionary.inDictionary(enteredText));
+        }
+      }
+    });
+  }
+
+  class gameTimer extends AnimationTimer
+  {
+    boolean gameStarted = false;
+    double conversionFactorFromNanoToSeconds = Math.pow(10,-9);
+    double currentTime;
+    double startupTime;
+    final private double totalTime = 5;
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Dictionary newDictionary = new Dictionary();
+    public void handle(long now)
+    {
+      if (gameStarted == true)
+      {
+        currentTime = (now * conversionFactorFromNanoToSeconds) - startupTime;
+      }
+      else
+      {
+        startupTime = now * conversionFactorFromNanoToSeconds;
+        gameStarted = true;
+      }
+      double timeRemaining = totalTime - currentTime;
+      timeRemaining = ((int) (timeRemaining * 10.0)) / 10.0;
 
-        GridPane boardPane = new GridPane();
-
-        int col = 0;
-        int row = 0;
-        for (Die Dice : newboard.getBoard()) {
-            boardPane.add(Dice.getImageView(), col, row);
-            if (col > 2) {
-                col = 0;
-                row++;
-            } else {
-                col++;
-            }
-        }
-
-        BorderPane mainGamePane = new BorderPane();
-        VBox gameVbox = new VBox();
-        HBox topHbox = new HBox();
-        VBox rightNumbers = new VBox();
-        VBox wrongNumbers = new VBox();
-        ScrollPane rightNumbersScroll = new ScrollPane();
-        ScrollPane wrongNumbersScroll = new ScrollPane();
-        rightNumbersScroll.setContent(rightNumbers);
-        wrongNumbersScroll.setContent(wrongNumbers);
-        Text scoreText = new Text("Score: " + score);
-        topHbox.getChildren().add(scoreText);
-        scoreText.setFont(Font.font(40));
-        TextField wordField = new TextField();
-        Button enterWord = new Button("enter");
-        gameVbox.getChildren().add(boardPane);
-        boardPane.setAlignment(Pos.CENTER);
-        gameVbox.getChildren().addAll(wordField, enterWord);
-        gameVbox.setAlignment(Pos.CENTER);
-        mainGamePane.setCenter(gameVbox);
-        mainGamePane.setTop(topHbox);
-        mainGamePane.setRight(rightNumbersScroll);
-        mainGamePane.setLeft(wrongNumbersScroll);
-        primaryStage.setScene(new Scene(mainGamePane, 1000, 800));
-        primaryStage.show();
-
-
-        enterWord.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                String enteredText;
-                enteredText = wordField.getText();
-                if (!enteredText.equals("")) {
-                    Text newWord = new Text(enteredText);
-                    newWord.setFont(Font.font(60));
-                    if (newboard.findWord(enteredText, 0, 0) && newDictionary.inDictionary(enteredText))
-                    {
-                        rightNumbers.getChildren().add(newWord);
-                        score += enteredText.length() - 2;
-                        scoreText.setText("Score: " + score);
-                    }
-                    else
-                    {
-                        wrongNumbers.getChildren().add(newWord);
-                    }
-                    System.out.println(newboard.findWord(enteredText, 0, 0));
-                    System.out.println(newDictionary.inDictionary(enteredText));
-                }
-            }
-        });
+      timerText.setText("Time Remaining: " + timeRemaining);
+      System.out.println(timeRemaining);
+      if (timeRemaining <= 0)
+      {
+        System.exit(0);
+      }
     }
+  }
 }
