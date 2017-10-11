acef10f2931a5d287b663a8148c9a73e6c6c5217
Mina Faltas <minas.here@gmail.com>
2017-10-11 00:49:39 -0600

    11th : QU letters and probability fixed; bug in game mode selection; bug in connecting letters of different rows

--- src/Board.java
+++ src/Board.java
@@ -2,112 +2,228 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
-public class Board {
-    private ArrayList<Die> board;
-    //direction array for 4x4
+public class Board
+{
+  private ArrayList<Die> board;
+  //direction array for 4x4
 //    private final int directionArray[] = {-5, -4, -3, -1, 1, 3, 4, 5};
-    //direction array for 5x5
-    private final int directionArray[] = {-6, -5 , -4, -1 , 1, 4, 5, 6};
+  //direction array for 5x5
+  private final int directionArray[] = {-6, -5, -4, -1, 1, 4, 5, 6};
 
-    Board() {
-        Die die0 = new Die(0, "AAAFRS");
-        Die die1 = new Die(1, "AAEEEE");
-        Die die2 = new Die(2, "AAFIRS");
-        Die die3 = new Die(3, "ADENNN");
-        Die die4 = new Die(4, "AEEEEM");
-        Die die5 = new Die(5, "AEEGMU");
-        Die die6 = new Die(6, "AEGMNN");
-        Die die7 = new Die(7, "AFIRSY");
-        Die die8 = new Die(8, "BJKQXZ");
-        Die die9 = new Die(9, "CCNSTW");
-        Die die10 = new Die(10, "CEIILT");
-        Die die11 = new Die(11, "CEILPT");
-        Die die12 = new Die(12, "CEIPST");
-        Die die13 = new Die(13, "DHHNOT");
-        Die die14 = new Die(14, "DHHLOR");
-        Die die15 = new Die(15, "DHLNOR");
-        Die die16 = new Die(15, "DDLNOR");
-        Die die17 = new Die(15, "EIIITT");
-        Die die18 = new Die(15, "EMOTTT");
-        Die die19 = new Die(15, "ENSSSU");
-        Die die20 = new Die(15, "FIPRSY");
-        Die die21 = new Die(15, "GORRVW");
-        Die die22 = new Die(15, "HIPRRY");
-        Die die23 = new Die(15, "NOOTUW");
-        Die die24 = new Die(15, "OOOTTU");
+  Board(boolean fourByFourBoolean)
+  {
+    board = new ArrayList<>();
 
-
-        board = new ArrayList<>();
-        board.add(die0);
-        board.add(die1);
-        board.add(die2);
-        board.add(die3);
-        board.add(die4);
-        board.add(die5);
-        board.add(die6);
-        board.add(die7);
-        board.add(die8);
-        board.add(die9);
-        board.add(die10);
-        board.add(die11);
-        board.add(die12);
-        board.add(die13);
-        board.add(die14);
-        board.add(die15);
-        board.add(die16);
-        board.add(die17);
-        board.add(die18);
-        board.add(die19);
-        board.add(die20);
-        board.add(die21);
-        board.add(die22);
-        board.add(die23);
-        board.add(die24);
-
-
-        for (Die dice : board) {
-            dice.rollDie();
+    if (fourByFourBoolean == true)
+    {
+      //4x4 version boggle
+      Die die0 = new Die(0, "AAEEGN");
+      board.add(die0);
+      Die die1 = new Die(1, "ELRTTY");
+      board.add(die1);
+      Die die2 = new Die(2, "AOOTTW");
+      board.add(die2);
+      Die die3 = new Die(3, "ABBJOO");
+      board.add(die3);
+      Die die4 = new Die(4, "EHRTVW");
+      board.add(die4);
+      Die die5 = new Die(5, "CIMOTU");
+      board.add(die5);
+      Die die6 = new Die(6, "DISTTY");
+      board.add(die6);
+      Die die7 = new Die(7, "EIOSST");
+      board.add(die7);
+      Die die8 = new Die(8, "DELRVY");
+      board.add(die8);
+      Die die9 = new Die(9, "ACHOPS");
+      board.add(die9);
+      Die die10 = new Die(10, "HIMNQU");
+      board.add(die10);
+      Die die11 = new Die(11, "EEINSU");
+      board.add(die11);
+      for (int i = 0; i < 10; i++)
+      {
+        if (die11.getTopLetter() != 'U')
+        {
+          die11.rollDie();
+        } else
+        {
+          break;
         }
-
-        Collections.shuffle(board);
-
-    }
-
-
-    boolean findWord(String word, int charInd, int boardInd) {
-        word = word.toUpperCase();
-        System.out.println("function called");
-        if (charInd == word.length() - 1) {
-            System.out.println("reached end of word\n");
-            return true;
-        } else if (charInd == 0) {
-            System.out.println("enterd first letter\n");
-            for (Die dice : board) {
-                System.out.println("top letter is " + dice.getTopLetter());
-                System.out.println("current char is " + word.charAt(charInd));
-                if (word.charAt(0) == dice.getTopLetter()) {
-                    charInd = 1;
-                    if (findWord(word, charInd, board.indexOf(dice))) return true;
-                }
-            }
-        } else if (charInd > 0) {
-            System.out.println("enterd second letter\n");
-            int tempBoardInd;
-            for (int i = 0; i < directionArray.length; i++) {
-                tempBoardInd = boardInd + directionArray[i];
-                if (tempBoardInd > 24 || tempBoardInd < 0) {
-                    continue;
-                }
-                if (word.charAt(charInd) == board.get(tempBoardInd).getTopLetter()) {
-                    charInd++;
-                    return findWord(word, charInd, tempBoardInd);
-                }
-            }
+      }
+      Die die12 = new Die(12, "EEGHNW");
+      board.add(die12);
+      Die die13 = new Die(13, "AFFKPS");
+      board.add(die13);
+      Die die14 = new Die(14, "HLNNRZ");
+      board.add(die14);
+      Die die15 = new Die(15, "DEILRX");
+      board.add(die15);
+    } else
+    {
+      //5x5 version boggle
+      Die die0 = new Die(0, "AAAFRS");
+      Die die1 = new Die(1, "AAEEEE");
+      Die die2 = new Die(2, "AAFIRS");
+      Die die3 = new Die(3, "ADENNN");
+      Die die4 = new Die(4, "AEEEEM");
+      Die die5 = new Die(5, "AEEGMU");
+      Die die6 = new Die(6, "AEGMNN");
+      Die die7 = new Die(7, "AFIRSY");
+      Die die8 = new Die(8, "BJKQXZ");
+      Die die9 = new Die(9, "CCNSTW");
+      Die die10 = new Die(10, "CEIILT");
+      Die die11 = new Die(11, "CEILPT");
+      Die die12 = new Die(12, "CEIPST");
+      Die die13 = new Die(13, "DHHNOT");
+      Die die14 = new Die(14, "DHHLOR");
+      Die die15 = new Die(15, "DHLNOR");
+      Die die16 = new Die(15, "DDLNOR");
+      Die die17 = new Die(15, "EIIITT");
+      Die die18 = new Die(15, "EMOTTT");
+      Die die19 = new Die(15, "ENSSSU");
+      Die die20 = new Die(15, "FIPRSY");
+      Die die21 = new Die(15, "GORRVW");
+      Die die22 = new Die(15, "HIPRRY");
+      Die die23 = new Die(15, "NOOTUW");
+      Die die24 = new Die(15, "OOOTTU");
+      for (int i = 0; i < 5; i++)
+      {
+        if (die24.getTopLetter() != 'U')
+        {
+          die24.rollDie();
+        } else
+        {
+          break;
         }
-        return false;
+      }
+
+      board.add(die0);
+      board.add(die1);
+      board.add(die2);
+      board.add(die3);
+      board.add(die4);
+      board.add(die5);
+      board.add(die6);
+      board.add(die7);
+      board.add(die8);
+      board.add(die9);
+      board.add(die10);
+      board.add(die11);
+      board.add(die12);
+      board.add(die13);
+      board.add(die14);
+      board.add(die15);
+      board.add(die16);
+      board.add(die17);
+      board.add(die18);
+      board.add(die19);
+      board.add(die20);
+      board.add(die21);
+      board.add(die22);
+      board.add(die23);
+      board.add(die24);
     }
 
-    ArrayList<Die> getBoard() {
-        return board;
+    for (Die dice : board)
+    {
+      if (dice.getTopLetter() != 'U' && dice.getTopLetter() != 'Q')
+      {
+        dice.rollDie();
+      }
     }
+
+    Collections.shuffle(board);
+
+    int qInd = -1;
+    int uInd = -1;
+    for (Die dice : board)
+    {
+      if (dice.getTopLetter() == 'Q')
+      {
+        qInd = board.indexOf(dice);
+
+        for (Die dieU : board)
+        {
+          if (dieU.getTopLetter() == 'U')
+          {
+            uInd = board.indexOf(dieU);
+          }
+        }
+      }
+    }
+
+    if (qInd != -1 && uInd != -1)
+    {
+      System.out.println("found q and u");
+      System.out.println("q is " + qInd);
+      System.out.println("u is " + uInd);
+      int tempInd;
+      for (int i = 0; i < directionArray.length; i++)
+      {
+        tempInd = qInd + directionArray[i];
+        System.out.println();
+
+        if (tempInd > board.size() - 1 || tempInd < 0)
+        {
+          continue;
+        } else
+        {
+          System.out.println("temp ind is " + tempInd);
+          Die dieU = board.get(uInd);
+          board.set(uInd, board.get(tempInd));
+          board.set(tempInd, dieU);
+          break;
+        }
+      }
+    }
+  }
+
+
+  boolean findWord(String word, int charInd, int boardInd)
+  {
+    word = word.toUpperCase();
+    System.out.println("function called");
+    if (charInd == word.length() - 1)
+    {
+      System.out.println("reached end of word\n");
+      return true;
+    } else if (charInd == 0)
+    {
+      System.out.println("enterd first letter\n");
+      for (Die dice : board)
+      {
+        System.out.println("top letter is " + dice.getTopLetter());
+        System.out.println("current char is " + word.charAt(charInd));
+        if (word.charAt(0) == dice.getTopLetter())
+        {
+          charInd = 1;
+          if (findWord(word, charInd, board.indexOf(dice))) return true;
+        }
+      }
+    } else if (charInd > 0)
+    {
+      System.out.println("enterd second letter\n");
+      int tempBoardInd;
+      for (int i = 0; i < directionArray.length; i++)
+      {
+        tempBoardInd = boardInd + directionArray[i];
+        if (tempBoardInd > 24 || tempBoardInd < 0)
+        {
+          continue;
+        }
+        if (word.charAt(charInd) == board.get(tempBoardInd).getTopLetter())
+        {
+          charInd++;
+          return findWord(word, charInd, tempBoardInd);
+        }
+      }
+    }
+    return false;
+  }
+
+  ArrayList<Die> getBoard()
+  {
+    return board;
+  }
 }
--- src/Controller.java
+++ src/Controller.java
@@ -6,6 +6,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
@@ -18,194 +19,248 @@ import javafx.stage.Stage;
 import java.awt.*;
 
 
-public class Controller extends Application {
+public class Controller extends Application
+{
+  public static void main(String[] args)
+  {
+    launch(args);
+  }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+  private BorderPane mainGamePane = new BorderPane();
+  private int score = 0;
+  private Text scoreText = new Text("Score: " + score);
+  private TextField wordField = new TextField();
+  private Text timerText = new Text();
+  private boolean drawLine = false;
+  private double timeRemaining;
+  //  private GridPane boardPane = new GridPane();
+  Point lastClicked = new Point(0, 0);
+  boolean fourByFourBoolean = false;
 
-    private Board newboard = new Board();
+  @Override
+  public void start(Stage primaryStage) throws Exception
+  {
+    Dictionary newDictionary = new Dictionary();
 
-    private BorderPane mainGamePane = new BorderPane();
-    private int score = 0;
-    private Text scoreText = new Text("Score: " + score);
-    private TextField wordField = new TextField();
+    //First scene: main menu
+    BorderPane mainMenuPane = new BorderPane();
+    VBox mainMenuBox = new VBox();
+    Button startGame = new Button("Start Game");
+    Button selectGameMode = new Button("Select Game Mode");
+    ComboBox gameMode = new ComboBox();
+    gameMode.getItems().addAll("4x4 mode", "5x5 mode");
+    gameMode.getSelectionModel().select(0);
+    mainMenuBox.getChildren().addAll(startGame, selectGameMode, gameMode);
+    mainMenuPane.setCenter(mainMenuBox);
+    mainMenuBox.setAlignment(Pos.CENTER);
+    primaryStage.setScene(new Scene(mainMenuPane, 400, 500));
+    primaryStage.show();
 
-    private Text timerText = new Text();
+//    System.out.println("four by four is " + fourByFourBoolean);
 
-    private boolean drawLine = false;
+    selectGameMode.setOnAction(new EventHandler<ActionEvent>()
+    {
+      @Override
+      public void handle(ActionEvent event)
+      {
+        if (gameMode.getValue() == "4x4 mode")
+        {
+          fourByFourBoolean = true;
 
-    private double timeRemaining;
-
-  private GridPane boardPane = new GridPane();
-
-    Point lastClicked = new Point(0,0);
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Dictionary newDictionary = new Dictionary();
-
-      Group boardGroup = new Group();
-        GridPane boardPane = new GridPane();
-        boardGroup.getChildren().add(boardPane);
-        Group secondBoardGroup = new Group();
-        boardGroup.getChildren().add(secondBoardGroup);
-
-        int col = 0;
-        int row = 0;
-        for (Die Dice : newboard.getBoard()) {
-            Button newLetterButton = new Button();
-            newLetterButton.setGraphic(Dice.getImageView());
-
-            newLetterButton.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {
-                  newLetterButton.setDisable(true);
-                    wordField.setText(wordField.getCharacters() + "" + Dice.getTopLetter());
-
-                    int x = (int) (((Button)(event.getSource())).getLayoutX() + 100);
-                    int y = (int) (((Button)(event.getSource())).getLayoutY() + 100);
-
-                  System.out.println("x is " + x + "y is " + y);
-                    if (drawLine == true)
-                    {
-                      Line arrow = new Line(lastClicked.getX(), lastClicked.getY(), x, y);
-                      secondBoardGroup.getChildren().add(arrow);
-                    }
-                    else
-                    {
-                      drawLine = true;
-                    }
-                    lastClicked.setLocation(x,y);
-                }
-            });
-            boardPane.add(newLetterButton, col, row);
-            if (col > 3) {
-                col = 0;
-                row++;
-            } else {
-                col++;
-            }
+        } else
+        {
+          fourByFourBoolean = false;
+          System.out.println("four by four is " + fourByFourBoolean);
         }
+      }
+    });
 
-        VBox gameVbox = new VBox();
-        HBox gameTextHbox = new HBox();
-        HBox topHbox = new HBox();
+    System.out.println("four by four is " + fourByFourBoolean);
+    Board newBoard = new Board(fourByFourBoolean);
 
-        VBox rightNumbers = new VBox();
-        VBox wrongNumbers = new VBox();
-        ScrollPane rightNumbersScroll = new ScrollPane();
-        ScrollPane wrongNumbersScroll = new ScrollPane();
-        Text rightNumbersText = new Text("Right Numbers: ");
-        rightNumbersText.setFont(Font.font(30));
-        Text wrongNumbersText = new Text("Wrong Numbers: ");
-        wrongNumbersText.setFont(Font.font(30));
-        rightNumbers.getChildren().add(rightNumbersText);
-        wrongNumbers.getChildren().add(wrongNumbersText);
-        rightNumbersScroll.setContent(rightNumbers);
-        wrongNumbersScroll.setContent(wrongNumbers);
-
-        HBox timerBox = new HBox();
-        timerBox.getChildren().add(timerText);
-        timerText.setFont(Font.font(40));
-        topHbox.getChildren().add(timerBox);
-        topHbox.getChildren().add(scoreText);
-        topHbox.setAlignment(Pos.CENTER_LEFT);
-        scoreText.setFont(Font.font(60));
-        Button enterWord = new Button("enter");
-        gameVbox.getChildren().add(boardGroup);
-        boardPane.setAlignment(Pos.CENTER);
-        gameTextHbox.getChildren().addAll(wordField, enterWord);
-        gameVbox.getChildren().add(gameTextHbox);
-        gameTextHbox.setAlignment(Pos.BASELINE_CENTER);
-        gameVbox.setAlignment(Pos.CENTER);
-        mainGamePane.setCenter(gameVbox);
-        mainGamePane.setTop(topHbox);
-        mainGamePane.setRight(rightNumbersScroll);
-        mainGamePane.setLeft(wrongNumbersScroll);
-
+    startGame.setOnAction(new EventHandler<ActionEvent>()
+    {
+      @Override
+      public void handle(ActionEvent event)
+      {
+        primaryStage.setScene(new Scene(mainGamePane, 1500, 1000));
         gameTimer newTimer = new gameTimer();
         newTimer.start();
 
-        primaryStage.setScene(new Scene(mainGamePane, 1500, 1000));
-        primaryStage.show();
+      }
+    });
 
-        enterWord.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-              for (int i = 0; i < boardPane.getChildren().size(); i++)
-              {
-                boardPane.getChildren().get(i).setDisable(false);
-              }
-                String enteredText;
-                enteredText = wordField.getText();
-                wordField.setText("");
-                secondBoardGroup.getChildren().clear();
-                drawLine = false;
-                if (!enteredText.equals("")) {
-                    Text newWord = new Text(enteredText);
-                    newWord.setFont(Font.font(20));
-                    if (newboard.findWord(enteredText, 0, 0) && newDictionary.inDictionary(enteredText)) {
-                        rightNumbers.getChildren().add(newWord);
-                        score += enteredText.length() - 2;
-                        scoreText.setText("Score: " + score);
-                    } else {
-                        wrongNumbers.getChildren().add(newWord);
-                    }
-                    System.out.println(newboard.findWord(enteredText, 0, 0));
-                    System.out.println(newDictionary.inDictionary(enteredText));
-                }
-            }
-        });
+    Group boardGroup = new Group();
+    GridPane boardPane = new GridPane();
+    boardGroup.getChildren().add(boardPane);
+    Group secondBoardGroup = new Group();
+    boardGroup.getChildren().add(secondBoardGroup);
+
+    int col = 0;
+    int row = 0;
+    for (Die Dice : newBoard.getBoard())
+    {
+      Button newLetterButton = new Button();
+      newLetterButton.setGraphic(Dice.getImageView());
+
+      newLetterButton.setOnAction(new EventHandler<ActionEvent>()
+      {
+        @Override
+        public void handle(ActionEvent event)
+        {
+          newLetterButton.setDisable(true);
+          wordField.setText(wordField.getCharacters() + "" + Dice.getTopLetter());
+
+          int x = (int) (((Button) (event.getSource())).getLayoutX() + 100);
+          int y = (int) (((Button) (event.getSource())).getLayoutY() + 100);
+
+          System.out.println("x is " + x + "y is " + y);
+          if (drawLine == true)
+          {
+            Line arrow = new Line(lastClicked.getX(), lastClicked.getY(), x, y);
+            secondBoardGroup.getChildren().add(arrow);
+          } else
+          {
+            drawLine = true;
+          }
+          lastClicked.setLocation(x, y);
+        }
+      });
+      boardPane.add(newLetterButton, col, row);
+      if (col > 3)
+      {
+        col = 0;
+        row++;
+      } else
+      {
+        col++;
+      }
     }
 
-    class gameTimer extends AnimationTimer {
-        boolean gameStarted = false;
-        double conversionFactorFromNanoToSeconds = Math.pow(10, -9);
-        double currentTime;
-        double startupTime;
-        final private double totalTime = 20;
+    VBox gameVbox = new VBox();
+    HBox gameTextHbox = new HBox();
+    HBox topHbox = new HBox();
 
-        @Override
-        public void handle(long now) {
-            if (gameStarted == true) {
-                currentTime = (now * conversionFactorFromNanoToSeconds) - startupTime;
-            } else {
-                startupTime = now * conversionFactorFromNanoToSeconds;
-                gameStarted = true;
-            }
-            timeRemaining = totalTime - currentTime;
-            timeRemaining = ((int) (timeRemaining * 10.0)) / 10.0;
+    VBox rightNumbers = new VBox();
+    VBox wrongNumbers = new VBox();
+    ScrollPane rightNumbersScroll = new ScrollPane();
+    ScrollPane wrongNumbersScroll = new ScrollPane();
+    Text rightNumbersText = new Text("Right Numbers: ");
+    rightNumbersText.setFont(Font.font(30));
+    Text wrongNumbersText = new Text("Wrong Numbers: ");
+    wrongNumbersText.setFont(Font.font(30));
+    rightNumbers.getChildren().add(rightNumbersText);
+    wrongNumbers.getChildren().add(wrongNumbersText);
+    rightNumbersScroll.setContent(rightNumbers);
+    wrongNumbersScroll.setContent(wrongNumbers);
 
-            timerText.setText("Time Remaining: " + timeRemaining + "         ");
-//      System.out.println(timeRemaining);
-          if (timeRemaining <= 10)
+    HBox timerBox = new HBox();
+    timerBox.getChildren().add(timerText);
+    timerText.setFont(Font.font(40));
+    topHbox.getChildren().add(timerBox);
+    topHbox.getChildren().add(scoreText);
+    topHbox.setAlignment(Pos.CENTER_LEFT);
+    scoreText.setFont(Font.font(60));
+    Button enterWord = new Button("enter");
+    gameVbox.getChildren().add(boardGroup);
+    boardPane.setAlignment(Pos.CENTER);
+    gameTextHbox.getChildren().addAll(wordField, enterWord);
+    gameVbox.getChildren().add(gameTextHbox);
+    gameTextHbox.setAlignment(Pos.BASELINE_CENTER);
+    gameVbox.setAlignment(Pos.CENTER);
+    mainGamePane.setCenter(gameVbox);
+    mainGamePane.setTop(topHbox);
+    mainGamePane.setRight(rightNumbersScroll);
+    mainGamePane.setLeft(wrongNumbersScroll);
+
+//    primaryStage.setScene(new Scene(mainGamePane, 1500, 1000));
+//    primaryStage.show();
+
+    enterWord.setOnAction(new EventHandler<ActionEvent>()
+    {
+      @Override
+      public void handle(ActionEvent event)
+      {
+        for (int i = 0; i < boardPane.getChildren().size(); i++)
+        {
+          boardPane.getChildren().get(i).setDisable(false);
+        }
+        String enteredText;
+        enteredText = wordField.getText();
+        wordField.setText("");
+        secondBoardGroup.getChildren().clear();
+        drawLine = false;
+        if (!enteredText.equals(""))
+        {
+          Text newWord = new Text(enteredText);
+          newWord.setFont(Font.font(20));
+          if (newBoard.findWord(enteredText, 0, 0) && newDictionary.inDictionary(enteredText))
           {
-            int k = (int) (timeRemaining % 2);
-            if (k == 0)
-            {
-              timerText.setFill(Color.GREEN);
-            }
-            else
-            {
-              timerText.setFill((Color.RED));
-            }
+            rightNumbers.getChildren().add(newWord);
+            score += enteredText.length() - 2;
+            scoreText.setText("Score: " + score);
+          } else
+          {
+            wrongNumbers.getChildren().add(newWord);
           }
-            if (timeRemaining <= 0) {
-                timerText.setText("Game Over                     ");
-                wordField.setDisable(true);
-                stop();
+          System.out.println(newBoard.findWord(enteredText, 0, 0));
+          System.out.println(newDictionary.inDictionary(enteredText));
+        }
+      }
+    });
+  }
 
-                BorderPane scorePane = new BorderPane();
-                Text finalScoreText = new Text("Game Over \n" + scoreText.getText());
-                finalScoreText.setFont(Font.font(60));
-                scorePane.setCenter(finalScoreText);
-                Stage scoreStage = new Stage();
-                scoreStage.setScene(new Scene(scorePane,1000,200));
-                scoreStage.show();
+  class gameTimer extends AnimationTimer
+  {
+    boolean gameStarted = false;
+    double conversionFactorFromNanoToSeconds = Math.pow(10, -9);
+    double currentTime;
+    double startupTime;
+    final private double totalTime = 20;
+
+    @Override
+    public void handle(long now)
+    {
+      if (gameStarted == true)
+      {
+        currentTime = (now * conversionFactorFromNanoToSeconds) - startupTime;
+      } else
+      {
+        startupTime = now * conversionFactorFromNanoToSeconds;
+        gameStarted = true;
+      }
+      timeRemaining = totalTime - currentTime;
+      timeRemaining = ((int) (timeRemaining * 10.0)) / 10.0;
+
+      timerText.setText("Time Remaining: " + timeRemaining + "         ");
+//      System.out.println(timeRemaining);
+      if (timeRemaining <= 10)
+      {
+        int k = (int) (timeRemaining % 2);
+        if (k == 0)
+        {
+          timerText.setFill(Color.GREEN);
+        } else
+        {
+          timerText.setFill((Color.RED));
+        }
+      }
+      if (timeRemaining <= 0)
+      {
+        timerText.setText("Game Over                     ");
+        wordField.setDisable(true);
+        stop();
+
+        BorderPane scorePane = new BorderPane();
+        Text finalScoreText = new Text("Game Over \n" + scoreText.getText());
+        finalScoreText.setFont(Font.font(60));
+        scorePane.setCenter(finalScoreText);
+        Stage scoreStage = new Stage();
+        scoreStage.setScene(new Scene(scorePane, 1000, 200));
+        scoreStage.show();
 
 //        System.exit(0);
-            }
-        }
+      }
     }
+  }
 }
